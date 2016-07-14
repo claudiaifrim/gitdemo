91f4689463ed68ea3e31ea58c2e1e6708ddbb336
Claudia Ifrim <ifrim.claudia@gmail.com>
2016-07-14 17:40:52 +0300

    add extra function

--- functions.js
+++ functions.js
@@ -1,7 +1,18 @@
-function sumAll() {
+function sum() {
     var i, sum = 0;
     for (i = 0; i < arguments.length; i++) {
         sum += arguments[i];
     }
     return sum;
+}
+
+function findMax() {
+    var i;
+    var max = -Infinity;
+    for(i = 0; i < arguments.length; i++) {
+        if (arguments[i] > max) {
+            max = arguments[i];
+        }
+    }
+    return max;
 }
